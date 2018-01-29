d9cfbf1eeafa696a6fa26df2736783f9acf862c8
andizq <andres.izquierdo@udea.edu.co>
2018-01-28 19:55:40 -0500

    Added Utils.py

---
+++ package/Utils.py
@@ -0,0 +1,20 @@
+#------------
+#CONSTANTS
+#------------
+
+MSun = 1.9891e30 #kg
+RSun = 696000000. #m
+LSun = 3.828e26 #J/s
+TSun = 5780. #K
+yr = 31557600 #s
+AU = 1.495978707e11 #m
+MSun_yr = MSun/yr #kg/s
+
+Mu = 1.6726219e-27 #kg ->proton mass
+G = 6.67384e-11 #(N m^2 kg^-2)Cavendish-gravitational constant
+sigma = 5.670373e-8 #(W m^-2 K^-4)Stefan-Boltzmann constant
+
+arcsecs = 206264.806247 #radians->arcsecs
+
+
+
