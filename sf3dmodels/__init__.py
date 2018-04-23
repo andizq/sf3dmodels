4361c9033498f3d4f9f020656920738456721775
andizq <andres.izquierdo@udea.edu.co>
2018-04-23 18:24:45 -0500

    init

--- sf3dmodels/__init__.py
+++ sf3dmodels/__init__.py
@@ -18,6 +18,9 @@ if sys.version_info < tuple((int(val) for val in "2.7".split('.'))):
 
 if not _ASTROPY_SETUP_:
     # For egg_info test builds to pass, put package imports here.
-
-    from .example_mod import *
+    
+    import Model
+    import Resolution as Res
+    import Plot_model
+    import Utils as U
 
