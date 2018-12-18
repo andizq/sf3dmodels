a8b549edb37f8783a65fe30e6d10224248472c64
andizq <andres.izquierdo@udea.edu.co>
2018-12-18 23:47:27 +0000

    spherical grid

--- sf3dmodels/grid/core.py
+++ sf3dmodels/grid/core.py
@@ -4,11 +4,17 @@ import numpy as np
 #GRID BUILDER
 #*************
 
-class Build_r(object):
-    _base = """
-    Computes the r - spherical coordinate on each grid point.
+class Spherical(object):
+    def __init__(self, GRID):
+        self.GRID = GRID
+    def set_flag(self, a):
+        self.GRID.flag[a] = True
 
-    .. math:: r = \\sqrt{x^2 + y^2 + z^2}
+class Build_r(Spherical):
+    _base = """
+    Computes the :math:`r` - spherical coordinate on each grid point.
+
+       .. math:: r = \\sqrt{x^2 + y^2 + z^2}
     """
     _pars = """
     Parameters
@@ -17,9 +23,9 @@ class Build_r(object):
        Grid structure to compute.
    
     get_r : bool
-       If True, stores the computed r coordinate in a new attribute ``GRID.r``
+       If True, stores the computed :math:`r` - coordinate as a new attribute ``GRID.r``.
 
-       Defaults to %r
+       Defaults to %r.
     """       
     _def0 = True
     _returns = """
@@ -28,23 +34,80 @@ class Build_r(object):
     New attributes:
     
     GRID.r : `numpy.ndarray`, shape (GRID.NPoints,)
-       Computed r - spherical coordinate.
+       Array of :math:`r` in the input units.
     """
     __doc__ = _base + _pars%_def0 + _returns
 
     def __init__(self, GRID, get_r=_def0):
-        self.GRID = GRID
+        super(Build_r,self).__init__(GRID)
         self._r_grid = np.linalg.norm(self.GRID.XYZ, axis = 0)
         self._r_max = np.max(self._r_grid)
-        if get_r: self.GRID.r = self._r_grid
+        if get_r: 
+            self.GRID.r = self._r_grid
+            self.set_flag('r')
+
+class Build_theta(Spherical):
+    _base = """
+    Computes :math:`\\theta` - polar angle - on each grid point.
+       .. math:: 
+          \\theta = \\cos^{-1}\\left(\\frac{z}{r}\\right) 
+    """
+    _pars = """
+    Parameters
+    ----------
+    GRID : `~sf3dmodels.Model.Struct`
+       Grid structure to compute.
+   
+    get_theta : bool
+       If True, stores the computed :math:`\\theta` - coordinate as a new attribute ``GRID.theta``.
+
+       Defaults to %r.
+    """       
+    _def0 = True
+    _returns = """
+    Returns
+    -------
+    New attributes:
     
-class Build_theta(object):
+    GRID.theta : `numpy.ndarray`, shape (GRID.NPoints,)
+       Array of angles in radians, in the range `[0, pi/2]` if :math:`z>=0` and 
+       `[pi/2, 0]` if :math:`z<0`; where :math:`\\theta=\\pi/2` for the plane :math:`z=0`.
+    """
+    __doc__ = _base + _pars%_def0 + _returns
+
     def __init__(self, GRID):
         self.GRID = GRID
         self.r_grid = np.linalg.norm(self.GRID.XYZ, axis = 0)
         self.r_max = np.max(self.r_grid)
         
-class Build_phi(object):
+class Build_phi(Spherical):
+    _base = """
+    Computes :math:`\\phi` - azimuthal angle - on each grid point. Uses `numpy.arctan2`.
+
+       .. math:: \\phi = \\tan^{-1}\\left(\\frac{y}{x}\\right) + 2\\pi
+    """
+    _pars = """
+    Parameters
+    ----------
+    GRID : `~sf3dmodels.Model.Struct`
+       Grid structure to compute.
+   
+    get_theta : bool
+       If True, stores the computed :math:`\\phi` - coordinate as a new attribute ``GRID.phi``.
+
+       Defaults to %r.
+    """       
+    _def0 = True
+    _returns = """
+    Returns
+    -------
+    New attributes:
+    
+    GRID.phi : `numpy.ndarray`, shape (GRID.NPoints,)
+       Array of angles in radians, in the range `[0, pi/2]`.
+    """
+    __doc__ = _base + _pars%_def0 + _returns
+
     def __init__(self, GRID):
         self.GRID = GRID
         self.r_grid = np.linalg.norm(self.GRID.XYZ, axis = 0)
