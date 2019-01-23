de80a29047b0afa3b6444b5533bccfddbfe26132
andizq <andres.izquierdo.c@gmail.com>
2019-01-23 20:22:43 +0000

    docs

--- docs/conf.py
+++ docs/conf.py
@@ -29,10 +29,10 @@ import datetime
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath('../..')) #Included by AFIC the 17th Dec of 2018
+#sys.path.insert(0, os.path.abspath('../..')) #Included by AFIC the 17th Dec of 2018
 
 try:
-    from sphinx_astropy.conf import *  # noqa
+    from sphinx_astropy.conf.v1 import *  # noqa
 except ImportError:
     print('ERROR: the documentation requires the sphinx-astropy package to be installed')
     sys.exit(1)
