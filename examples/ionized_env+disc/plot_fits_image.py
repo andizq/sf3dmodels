7470e377f5a7f8f8c88d9c7ad96d5b240b62146d
andizq <andres.izquierdo.c@gmail.com>
2019-04-05 14:31:09 +0200

    cleaning up

--- examples/ionized_env+disc/plot_fits_image.py
+++ examples/ionized_env+disc/plot_fits_image.py
@@ -2,13 +2,7 @@ import radmc3dPy.image as image
 import matplotlib.pyplot as plt
 import numpy as np
 
-i = 7
-dist = 4000.
-#lambdas = np.array([ 37500., 20000., 13043.47826087, 9375., 7142.85714286, 
-#                     5454.54545455, 4285.71428571, 3529.41176471])
-#en GHz: np.array([8,15,23,32,42,55,70,85])*1. GHz.
-
-
+dist = 4000. #pc
 im = image.readImage()
 
 #image.plotImage(im, au=True, log=True, maxlog=10, saturate=1e-5, cmap=plt.cm.gist_heat)
@@ -20,5 +14,3 @@ image.plotImage(cim, arcsec=True, dpc=dist, log=True, maxlog=10, bunit='snu', cm
 """
 im.writeFits('img_HII_env+disc.fits', dpc=dist)#, coord='03h10m05s -10d05m30s')
 
-
-#Mirar lo de inputs desde terminal
