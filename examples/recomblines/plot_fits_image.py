b48ef7e9399936bc6f70b01ec852aac4e81b3a6e
andizq <andres.izquierdo.c@gmail.com>
2019-04-05 19:41:41 +0200

    plot_fits_image.py + img_channel.png

--- examples/recomblines/plot_fits_image.py
+++ examples/recomblines/plot_fits_image.py
@@ -1,6 +1,4 @@
 import radmc3dPy.image as image
-import matplotlib.pyplot as plt
-import numpy as np
 import subprocess
 
 output = 'img_rl_powerlaw.fits'
@@ -9,8 +7,7 @@ subprocess.call(['rm',output])
 dist = 2000.
 im = image.readImage()
 
-data = im.image #Accessing image data
-plt.imshow(data[:,:,29], origin='lower', cmap='cool') #plotting a single channel
-plt.show()
+data = im.image #Image data, channels lie on axis 2. 
 
+image.plotImage(im, dpc=dist, au=True, bunit='jy/pixel', ifreq=29, cmap='gnuplot') #plotting channel 29
 im.writeFits(fname=output, dpc=dist, coord='03h10m05s -10d05m30s') #writting 3d fits cube
