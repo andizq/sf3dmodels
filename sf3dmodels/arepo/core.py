b498d2f06be398cdd78169f8ba7728734948ab2f
andizq <andres.izquierdo.c@gmail.com>
2019-06-07 22:21:43 +0100

    comments in UniqueCells.mergemass

--- sf3dmodels/arepo/core.py
+++ sf3dmodels/arepo/core.py
@@ -91,10 +91,10 @@ class UniqueCells(object):
         for prop in self.data: self.data[prop][0:ngas] = self.data[prop][id_sort]  
         data_mass = self.data['mass']
 
-        _,id_pos = np.unique(r, return_index = True) #First guess of unique points. The returned indices are sorted by less to greater r
+        _,id_pos = np.unique(r, return_index = True) #First guess of unique points. The returned indices are sorted from lower to greater r
 
         ind_diff = id_pos[1:] - id_pos[:-1] #Difference between item i+1 - item i to look for indices holes in the array
-        ind_diff = np.append(ind_diff, ngas - id_pos[-1]) #Appending difference of last item
+        ind_diff = np.append(ind_diff, ngas - id_pos[-1]) #Appending the difference of the last item
     
         ind_holes, = np.where(ind_diff > 1) #Holes indicate repeated cells
         #ind_noholes, = np.where(ind_diff == 1)
@@ -102,24 +102,25 @@ class UniqueCells(object):
         id_new = []; new_list = []; merged_mass = []
         for ind in ind_holes: #iterates over holes
             id0 = id_pos[ind]
-            for id in np.arange(id0+1, id0+ind_diff[ind]): #iterate over neighbor holes to check which of them are identical to id0
+            for id in np.arange(id0+1, id0+ind_diff[ind]): #iterates over neighbouring holes to check which of them are identical to id0
                 if (li_x[id0],li_y[id0],li_z[id0]) == (li_x[id],li_y[id],li_z[id]): #if identical merge mass into id0
                     data_mass[id0] += data_mass[id]            
                 else: #if not...
                     new_atall = True
                     for ch in np.arange(id0+1, id): #check the hole 'id' against the previous holes (!= id0) to avoid accepting duplicates
-                        new = (li_x[ch],li_y[ch],li_z[ch]) != (li_x[id],li_y[id],li_z[id]) #True if different to ch
+                        new = (li_x[ch],li_y[ch],li_z[ch]) != (li_x[id],li_y[id],li_z[id]) #True if different to the ch-th point
                         new_list.append(new)
                         new_atall &= new  
                     if new_atall: id_new.append(id) #True if new=True when compared to all the previous holes. Then the hole 'id' is a new point that must not be rejected
-                    else: #Means id is repeated against one (or more than one) of its previous neighboring holes 
-                          # (holes that in fact should have been added one step before to the list 'id_new' of new ids). 
+                    else: #Means that id is repeated against one (or more than one) of its previous neighbouring holes 
+                          # (holes that in fact were added one step before to the list 'id_new' of new ids). 
                           #  Then, merge the mass of the repeated holes.
-                        for i_n in range(len(new_list)): #Iterates over the list of neighboring holes that stores whether the point id is identical to them or not
+                        for i_n in range(len(new_list)): #Iterates over the list of neighbouring holes that stores whether the point id is identical to them or not
                             new_point = new_list[i_n]
                             if not new_point: #True if the hole is twin of id 
                                 data_mass[id0+1 + i_n] += data_mass[id]
-                                break #Only merge with the first repeated hole which is the only one alive.
+                                #for prop in self.data: print (id, prop, self.data[prop][id0+1+i_n], self.data[prop][id])
+                                break #Only merge into the first repeated hole which is the only one alive up to this point
                     new_list = []
 
         id_new = np.array(id_new)
