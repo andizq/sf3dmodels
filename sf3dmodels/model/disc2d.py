591a7e15417ab013c1335c7f315e7e50aa766769
andizq <andres.izquierdo@udea.edu.co>
2020-03-30 00:58:02 +0200

    Added Linewidth class and allowing to compute Jv by integrating in channels width

--- sf3dmodels/model/disc2d.py
+++ sf3dmodels/model/disc2d.py
@@ -440,11 +440,27 @@ class Intensity:
     @staticmethod
     def line_profile_temp(v_chan, v, T, v_turb=0.0, mmol=2*u.amu):
         v_sigma = np.sqrt(2*kb*T/mmol + v_turb**2) * 1e-3 #in km/s
-        return 1/(np.sqrt(np.pi)*v_sigma) * np.exp(-((v-v_chan)/v_sigma)**2)
+        #return 1/(np.sqrt(np.pi)*v_sigma) * np.exp(-((v-v_chan)/v_sigma)**2)
+        return np.exp(-((v-v_chan)/v_sigma)**2)
 
     @staticmethod
     def line_profile_v_sigma(v_chan, v, v_sigma, mmol=2*u.amu):
-        return 1/(np.sqrt(np.pi)*v_sigma) * np.exp(-((v-v_chan)/v_sigma)**2)
+        #return 1/(np.sqrt(np.pi)*v_sigma) * np.exp(-((v-v_chan)/v_sigma)**2)
+        return np.exp(-((v-v_chan)/v_sigma)**2)
+
+    @staticmethod
+    def line_profile_full(v_chan, v, v_sigma, channel_width):
+        half_chan = 0.5*channel_width
+        v0 = v_chan - half_chan
+        v1 = v_chan + half_chan
+        nsub = 100
+        vsub = np.linspace(v0, v1, nsub)
+        dvsub = vsub[1]-vsub[0]
+        J = 0
+        for vs in vsub:
+            J += np.exp(-((v-vs)/v_sigma)**2)
+        J = J * dvsub/channel_width
+        return J
 
     def get_channel(self, velocity2d, intensity2d, temperature2d, v_chan, **kwargs):                    
         vel2d, temp2d, int2d = velocity2d, {}, {}
@@ -460,8 +476,8 @@ class Intensity:
         v_far_clean = np.where(np.isnan(v_far), -np.inf, v_far)
         
         #int2d_near = int2d['near'] * v_near_clean / v_near_clean.max()
-        int2d_near = np.where(np.isnan(int2d['near']), -np.inf, int2d['near'] * v_near_clean / v_near_clean.max())
-        int2d_far = np.where(np.isnan(int2d['far']), -np.inf, int2d['far'] * v_far_clean / v_far_clean.max())
+        int2d_near = np.where(np.isnan(int2d['near']), -np.inf, int2d['near'] * v_near_clean)# / v_near_clean.max())
+        int2d_far = np.where(np.isnan(int2d['far']), -np.inf, int2d['far'] * v_far_clean)# / v_far_clean.max())
         
         vmap_full = np.array([v_near_clean, v_far_clean]).max(axis=0)
         int2d_full = np.array([int2d_near, int2d_far]).max(axis=0)
